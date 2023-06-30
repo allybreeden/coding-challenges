6d7ccebee9723765e5cb2ce93b33bb82c7764c53
allybreeden <allybreeden@gmail.com>
2023-06-30 11:01:19 -0500

    Completed Coding Challenge #1

---
+++ one.js
@@ -0,0 +1,13 @@
+console.log("Coding Challenge One");
+
+function triArea(base, height) {
+    let area = (base * height)/2;
+    return area;
+}
+
+console.log(triArea(3,2));
+
+console.log(triArea(7,4));
+
+console.log(triArea(10,10));
+
